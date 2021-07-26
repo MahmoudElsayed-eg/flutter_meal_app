bac2b7a7f606fb94673fc0568a909b95004332d6
sorsar <sorsar1@yahoo.com>
2021-07-26 17:53:14 +0200

    navigate to meals_screen.dart

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/screens/meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -8,22 +9,33 @@ class CategoryItem extends StatelessWidget {
   const CategoryItem({Key? key, required this.title, required this.color})
       : super(key: key);
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return MealScreen();
+    } ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
-      child: Center(
-          child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-        textAlign: TextAlign.center,
-      )),
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      onTap:() => selectCategory(context),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
+        child: Center(
+            child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+          textAlign: TextAlign.center,
+        )),
+      ),
     );
   }
 }
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealScreen extends StatelessWidget {
+  const MealScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("gg"),),
+        body: Center(
+            child: Container(
+      child: Text("haha"),
+    )));
+  }
+}
