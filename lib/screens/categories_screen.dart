3131ff48630136499d14490ddd537d5f8ec20eb3
sorsar <sorsar1@yahoo.com>
2021-07-26 16:49:08 +0200

    removed the home screen and replaced it with categories_screen.dart and some ui changes

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/category_item.dart';
 import 'package:flutter_meal_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -6,16 +7,18 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: GridView(
+    return Scaffold(appBar: AppBar(title: const Text("Meal App"),),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: GridView(
           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 200,
               childAspectRatio: 3 / 2,
               crossAxisSpacing: 20,
-              mainAxisSpacing: 20),children: DUMMY_CATEGORIES.map((e) {
-                return Container(color: e.color,child: Center(child: Text(e.title)),);
-      }).toList(),),
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES.map((e) => CategoryItem(title: e.title, color: e.color)).toList(),
+        ),
+      ),
     );
   }
 }
